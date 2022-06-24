761af6f6d338068c5340d5b23adf66902cd2aef5
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-24 17:04:28 -0500

    setup iquirer

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+
+
+This is a Node.js program that generates a README page based on the users input.
+
+## Image
+
+<!-- ![Screenshot of main application](./images/Screenshot.jpg) -->
+
+
+## Review
+
+* GitHub repository: https://github.com/willig0203/Portfolio-Generator
+
+
+## Contribution
+
+Gary Williams (Trademark GarywareLLC).
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,28 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init(prompt) {}
+const init = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
+      },
+
+    ]);
+  };
 
 // Function call to initialize app
 init();
+
+console.log("hello");
+console.log(inquirer);
