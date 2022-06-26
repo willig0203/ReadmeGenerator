f7c7046a59ae12011003b912c7817aca7e71ee98
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-06-25 21:32:43 -0500

    try license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,10 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+const renderLicenseBadge = license => {
+  if (license !== "None") {
+    return "[![License](https://img.shields.io/badge/license-MIT-green)](./LICENSE)";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,14 +18,15 @@ function renderLicenseSection(license) { }
 module.exports = data => {
 
   return `
+# license badge: ${renderLicenseBadge(data.license)}
   # Project Title: ${data.title}
   ## Description: ${data.description}
   ## Installation: ${data.installation}
-  ## Program Usage: ${data.usage}
+  ## Usage: ${data.usage}
   ## Want to contribute: ${data.contribution}
-  ## How to test: ${data.test}
-  ## License: ${data.license}
-  ## Git user name: ${data.gitUsername}
+  ## Testing: ${data.test}
+  ## License:  ${data.license}
+  ## Git Username: ${data.gitUsername}
   ## Email: ${data.email}
 `;
 
